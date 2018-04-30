c09c4298f861c349767c95d308dd4aeaefd7c394
Alexander Dreweke <alexander@dreweke.net>
2018-04-30 18:16:11 +0200

    update headers

--- src/main/java/de/shadowhunt/maven/plugins/packageinfo/PackageInfoPlugin.java
+++ src/main/java/de/shadowhunt/maven/plugins/packageinfo/PackageInfoPlugin.java
@@ -1,6 +1,6 @@
 /**
  * Maven package-info.java Plugin - Autogenerates package-info.java files with arbitrary headers
- * Copyright © 2012 - 2017 shadowhunt (dev@shadowhunt.de)
+ * Copyright © ${project.inceptionYear} shadowhunt (dev@shadowhunt.de)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
